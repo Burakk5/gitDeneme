8f1e86aef8c4375e217e4ff3b0ae0e30c14e295f
BK WITHER <bkwither@gmail.com>
2021-08-05 16:17:50 +0300

    Add project files.

---
+++ git deneme yine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_deneme_yine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
